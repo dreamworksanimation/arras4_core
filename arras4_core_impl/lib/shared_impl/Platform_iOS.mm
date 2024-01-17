d21f0df74f29b430fa3acbee896cdb4158eb47ba
Jon Lanz <Jon.Lanz@dreamworks.com>
2024-01-17 14:14:50 -0800

    Update license for 2024 (#22)

--- arras4_core_impl/lib/shared_impl/Platform_iOS.mm
+++ arras4_core_impl/lib/shared_impl/Platform_iOS.mm
@@ -1,4 +1,4 @@
-// Copyright 2023 DreamWorks Animation LLC
+// Copyright 2023-2024 DreamWorks Animation LLC
 // SPDX-License-Identifier: Apache-2.0
 
 #import <UIKit/UIKit.h>
@@ -14,9 +14,9 @@ namespace arras
         NSString* model = [[UIDevice currentDevice] model];
         NSString* systemName = [[UIDevice currentDevice] systemName];
         NSString* systemVersion = [[UIDevice currentDevice] systemVersion];
-        
+
         NSLog(@"Name: %@\nModel: %@\nSystem Name: %@\nSystem Version: %@\n", name, model, systemName, systemVersion);
-        
+
         aInfo.mPlatformType = [name UTF8String];
         aInfo.mPlatformModel = [model UTF8String];
         aInfo.mOSName = [systemName UTF8String];
